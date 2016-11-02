6cdf54a65ef89dae641dba3caad2ccd93e188d31
Mathias Bachmann <mba@facelift-bbt.com>
2016-11-02 09:41:48 +0100

    + updated setting structure for better an better overview of the apps and tools + removed deprecated stuff for Ansible + updated apps list + renamed brews to tools + clean up

--- run.sh
+++ run.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-if xcode-select --install 2>&1 | grep installed
+
+if [[ `xcode-select --install 2>&1 | grep installed` ]]
 then
 	echo 'Up&Run | Xcode already installed'
-else
-	echo 'Up&Run | Install Xcode'
-fi
-
-read -r -p "Xcode installed? Ready to start Up&Run [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    if ! [ `which brew` ]; then
-    	echo 'Up&Run | Install Homebrew'
-    	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
-    	
-    	echo 'Up&Run | Updating Homebrew...'
-		brew update	&> /dev/null
-		brew tap caskroom/versions &> /dev/null
-    fi
-
-    if [ `which brew` ]; then
-		if ! [ `brew ls --versions ansible` ]; then
-			echo 'Up&Run | Installing Ansible...'
-			brew install ansible &> /dev/null
+	read -r -p "Ready to start Up&Run [y/N] " response
+	if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+	    if ! [[ `which brew` ]]; then
+	    	echo 'Up&Run | Install Homebrew'
+	    	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+	    	
+	    	echo 'Up&Run | Updating Homebrew...'
+			brew update	&> /dev/null
+			brew tap caskroom/versions &> /dev/null
+	    fi
+	
+	    if [[ `which brew` ]]; then
+			if ! [[ `brew ls --versions ansible` ]]; then
+				echo 'Up&Run | Installing Ansible...'
+				brew install ansible &> /dev/null
+			fi
+		fi
+	
+	    if [[ `which ansible-playbook` ]]; then
+			echo 'Up&Run | Run Ansible Playbook'
+			ansible-playbook -i upandrun/hosts upandrun/playbook.yml
 		fi
 	fi
-
-    if [ `which ansible-playbook` ]; then
-		echo 'Up&Run | Run Ansible Playbook'
-		ansible-playbook -i upandrun/hosts upandrun/playbook.yml
-	fi
+else
+	echo 'Up&Run | Please install Xcode and try again.'
 fi
 exit;
